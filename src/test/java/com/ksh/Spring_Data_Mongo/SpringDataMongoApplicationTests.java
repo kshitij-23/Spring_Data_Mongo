72ab2538b00b8adfb280f0a081b0eb0e39dcac96
solanki078@gmail.com <solanki078@gmail.com>
2018-12-24 09:59:02 +0530

    Project Setup

---
+++ src/test/java/com/ksh/Spring_Data_Mongo/SpringDataMongoApplicationTests.java
@@ -0,0 +1,17 @@
+package com.ksh.Spring_Data_Mongo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringDataMongoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
